e5c704c7a99d07a2fef42398d36bd2912c7e9af8
Abhinay Dev Berma <abhinaydevberma@gmail.com>
2021-11-09 19:58:40 +0530

    updated

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import express from 'express';
-
-
+import fs from 'fs'
 const app = express();
 const PORT = process.env.PORT || 47;
 
+app.get('/',(req,res)=>{
+    console.log("TEST");
+    res.send("Please request your name.");
+});
 
-
-app.get('/get',(req,res)=>{
+app.get('/sonal',(req,res)=>{
     console.log("TEST");
     res.send("I Love You Ji....!!");
 });
-
+app.get('/anjali',(req,res)=>{
+    console.log("TEST");
+    res.send("I Love You....:))");
+});
+app.get('/sachin',(req,res)=>{
+    console.log("TEST");
+    res.send("Love You Beta Ji....!");
+});
 app.listen(PORT,() => console.log(`Server running on port: http://localhost:${PORT}`));
