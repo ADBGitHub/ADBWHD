4cbd6ba5e91e4ad8c244caecf0eadf64e59839ad
Abhinay Dev Berma <abhinaydevberma@gmail.com>
2021-11-09 09:19:16 +0530

    updated

--- index.js
+++ index.js
@@ -5,13 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 47;
 var fs = fs();
 
-app.get('/',(req,res)=>{
-    fs.readFile('./index.html',function(error,html){
-        if(error) throw error;
-    res.writeHead(200, {"Content-Type":"text/html"});
-    res.write(html);
-    res.end();
-});
 
 app.get('/get',(req,res)=>{
     console.log("TEST");
