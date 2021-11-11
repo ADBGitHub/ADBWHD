eef40cd58983d41e18f1ec9e545fb75034631948
Abhinay Dev Berma <abhinaydevberma@gmail.com>
2021-11-11 15:10:34 +0530

    updated

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
+import Router from 'Router';
+
 const app = express();
 const PORT = process.env.PORT || 47;
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }));
-app.post('/',(req,res)=>{
+app.post('/', (req, res) => {
     var api = req.body.api_key;
-    if(api == "adbwhdapi"){res.send("Connected!");}else{res.send("Error");}    
+    if (api == "adbwhdapi") { res.send("Connected!"); } else { res.send("Error"); }
 });
 
-app.get('/sonal',(req,res)=>{
+app.get('/sonal', (req, res) => {
     console.log("TEST");
     res.send("I Love You Ji....!!");
 });
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     console.log("TEST");
     res.send("Please request your name.");
 });
-app.get('/ambika',(req,res)=>{
+app.get('/ambika', (req, res) => {
     console.log("TEST");
     res.send("Love You re....!!");
 });
-app.get('/anjali',(req,res)=>{
+app.get('/anjali', (req, res) => {
     console.log("TEST");
     res.send("I Love You....:))");
 });
-app.get('/sachin',(req,res)=>{
+app.get('/sachin', (req, res) => {
     console.log("TEST");
     res.send("Love You Beta Jii....!");
 });
-app.get('/samiksha',(req,res)=>{
+app.get('/samiksha', (req, res) => {
     console.log("TEST");
     res.send("Hello Samiksha.....!!");
 });
-app.listen(PORT,() => console.log(`Server running on port: http://localhost:${PORT}`));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
