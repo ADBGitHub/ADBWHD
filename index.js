790137667da18fd4a7d271d5cf6cedc6c1f03f51
Abhinay Dev Berma <abhinaydevberma@gmail.com>
2021-11-11 08:03:01 +0000

    updated

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-
-const express = require("express");
-const bodyParser = require("body-parser")
-const PORT = process.env.PORT || 47;
+import express from 'express';
 const app = express();
-app.use(bodyParser.urlencoded({
-    extended:true
-}));
+
+const PORT = process.env.PORT || 47;
+
 app.post('/',(req,res)=>{
     var api = req.body.api_key;
     console.log(api);
