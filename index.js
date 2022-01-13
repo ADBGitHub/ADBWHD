1398387fda75faaf5cab03f4ebb8bf15de249916
Abhinay Dev Berma <abhinaydevberma@gmail.com>
2022-01-13 05:55:59 +0530

    updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/sonal", (req, res) => {
   console.log("TEST");
   res.send("I Love You Ji....!!");
 });
-app.get("/", (req, res) => {
+app.get("/sachin", (req, res) => {
   console.log("TEST");
   res.send("I Love You Ji....!!");
 });
@@ -35,13 +35,13 @@ app.get("/samiksha", (req, res) => {
   console.log("TEST");
   res.send("Hello Samiksha.....!!");
 });
-// app.get('/', (req, res) => {
-//     fs.readFile('./index.html',function(err,data){
-//         res.writeHead(200,{'Content-Type':'text/html'});
-//         res.write(data);
-//         return res.end();
-//     });
-// });
+app.get("/", (req, res) => {
+  fs.readFile("./index.html", function (err, data) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    return res.end();
+  });
+});
 app.listen(PORT, () =>
   console.log(`Server running on port: http://localhost:${PORT}`)
 );
