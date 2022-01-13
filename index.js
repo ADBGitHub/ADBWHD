8100f6b755f4723b33d39454e3e047c7f19061cf
Abhinay Dev Berma <abhinaydevberma@gmail.com>
2022-01-13 06:44:31 +0530

    updated

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import fs from "fs";
 
+const http = require("http");
 const app = express();
 const PORT = process.env.PORT || 47;
 const Router = express.Router();
+var switch1 = 0;
+var switch2 = 0;
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
+http.get("url", (res) => {
+  let data = "";
+  res.on("data", (chunk) => {
+    data += chunk;
+  });
+
+  res.on("end", () => {
+    console.log(data);
+  });
+});
 app.get("/sonal", (req, res) => {
   console.log("TEST");
   res.send("I Love You Ji....!!");
@@ -41,6 +54,10 @@ app.get("/", (req, res) => {
     res.write(data);
     return res.end();
   });
+});
+app.get("/onSwitch1", (req, res) => {
+  console.log("TEST");
+  res.send("Hello Samiksha.....!!");
 });
 app.listen(PORT, () =>
   console.log(`Server running on port: http://localhost:${PORT}`)
