3769b3d809f3f03584c6a89e12f55dae45b47f0b
Abhinay Dev Berma <abhinaydevberma@gmail.com>
2022-06-08 16:30:06 +0530

    updated

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   fs.readFile("./index.html", function (err, data) {
+    data.replace("{%Abhinay%}", "DEV BERMA");
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(data);
     return res.end();
