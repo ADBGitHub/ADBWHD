c94ea120c7dd02d1d307ce8b6fa1a82452d7accd
Abhinay Dev Berma <abhinaydevberma@gmail.com>
2022-06-14 21:34:45 +0530

    updated

--- index.js
+++ index.js
@@ -6,45 +6,7 @@ const app = express();
 app.set("view engine", "ejs");
 const PORT = process.env.PORT || 47;
 const Router = express.Router();
-let switch1 = 0;
-let switch2 = 0;
-let switch3 = 0;
-let switch4 = 0;
-let switch5 = 0;
-let switch6 = 0;
-let switch7 = 0;
 
-let switch8 = 0;
-let switch9 = 0;
-let switch10 = 0;
-let switch11 = 0;
-
-let cs1Val = 0.0;
-let cs2Val = 0.0;
-let vsVal = 0.0;
-
-let prvTime = 0.0;
-let crtTime = 0.0;
-let prvPowr = 0.0;
-let powr = 0.0;
-let energy = 0.0;
-
-let sensor = 0;
-let door = 0;
-let doorLock = 0;
-let door1 = 0;
-let doorLock1 = 0;
-let seq = 0;
-let seq1 = 0;
-let gasLeakage = 0;
-let smoke = 0;
-let fire = 0;
-let window = "";
-let temp = 0;
-let Irms = 0;
-let Vrms = 0;
-let Powr = 0;
-let warning2 = "No Warning...!!";
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -53,7 +15,12 @@ app.use(
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(express.static("views"));
+
+let switch1 = 0;
+let switch2 = 0;
+
 let hf = fs.readFileSync("./views/HAMP.ejs", "utf-8");
+
 app.get("/", (req, res) => {
   res.render("HAMP");
 });
@@ -144,6 +111,44 @@ app.get("/offSwitch2", (req, res) => {
   res.send("switch 2 is off");
 });
 
+app.get("/status", (req, res) => {
+  if (switch1 == 0 && switch2 == 0) {
+    res.send("0");
+  } else if (switch1 == 1 && switch2 == 0) {
+    res.send("10");
+  } else if (switch1 == 1 && switch2 == 1) {
+    res.send("11");
+  } else if (switch1 == 0 && switch2 == 1) {
+    res.send("1");
+  }
+});
+
+app.get("/updateSwitch11", (req, res) => {
+  switch1 = 1;
+  switch2 = 1;
+  res.send("updated");
+});
+app.get("/updateSwitch10", (req, res) => {
+  switch1 = 1;
+  switch2 = 0;
+  res.send("updated");
+});
+app.get("/updateSwitch01", (req, res) => {
+  switch1 = 0;
+  switch2 = 1;
+  res.send("updated");
+});
+app.get("/updateSwitch00", (req, res) => {
+  switch1 = 0;
+  switch2 = 0;
+  res.send("updated");
+});
+
+/* <-----------------Irigation System Sachin --------------------------> */
+
+let switch3 = 0;
+let sensor = 0;
+
 app.get("/onSwitch3", (req, res) => {
   switch3 = 1;
   res.send("Pump is ON");
@@ -161,6 +166,97 @@ app.get("/offSensor", (req, res) => {
   res.send("sensor 2 is off");
 });
 
+app.get("/status1", (req, res) => {
+  if (switch3 == 0 && sensor == 0) {
+    res.send("0");
+  } else if (switch3 == 1 && sensor == 0) {
+    res.send("10");
+  } else if (switch3 == 1 && sensor == 1) {
+    res.send("11");
+  } else if (switch3 == 0 && sensor == 1) {
+    res.send("1");
+  }
+});
+
+/* <-------------------Door Lock System-------------------------------------> */
+let door = 0;
+let doorLock = 0;
+let seq = 0;
+
+app.get("/doorIsClosed", (req, res) => {
+  door = 1;
+  res.send("Door is Closed");
+});
+app.get("/doorIsOpen", (req, res) => {
+  door = 0;
+  res.send("Door is Open");
+});
+app.get("/doorStatus", (req, res) => {
+  res.send(door.toString());
+});
+app.get("/lockStatus", (req, res) => {
+  res.send(doorLock.toString());
+});
+app.get("/openLock", (req, res) => {
+  if (Boolean(seq)) {
+    res.send("Unlock Door..");
+    doorLock = 1;
+  } else {
+    doorLock = 0;
+    res.send("Lock is Open");
+  }
+});
+app.get("/closelock", (req, res) => {
+  doorLock = 1;
+  res.send("Lock is Close");
+});
+app.get("/checkSeq", (req, res) => {
+  res.send(seq.toString());
+});
+app.get("/onSeq", (req, res) => {
+  seq = 1;
+  res.send("LOCKED....!!");
+});
+app.get("/offSeq", (req, res) => {
+  seq = 0;
+  res.send("UNLOCKED....!!");
+});
+app.get("/warning", (req, res) => {
+  if (Boolean(seq) && !Boolean(door)) {
+    res.send("!!...Security is broken...!!");
+  }
+  res.send("All Ok...!!");
+});
+
+/* <------------------Home Sequrity-----------------------------------------> */
+let gasLeakage = 0;
+let smoke = 0;
+let fire = 0;
+let window = "";
+app.get("/HomeStatus", (req, res) => {
+  gasLeakage = req.query.gasLeakage;
+  smoke = req.query.smoke;
+  fire = req.query.fire;
+  res.send(window);
+});
+app.get("/HomeStatus1", (req, res) => {
+  res.send(gasLeakage + smoke + fire);
+});
+app.get("/openWindow", (req, res) => {
+  window = "openWindow";
+  res.send("Window is open");
+});
+app.get("/closeWindow", (req, res) => {
+  window = "closeWindow";
+  res.send("Window is Close");
+});
+
+/* <-----------------Door Lock + Home Automation----------------------------> */
+let switch4 = 0;
+let switch5 = 0;
+let door1 = 0;
+let doorLock1 = 0;
+let seq1 = 0;
 app.get("/onSwitch4", (req, res) => {
   switch4 = 1;
   res.send("switch 1 is on");
@@ -177,6 +273,95 @@ app.get("/offSwitch5", (req, res) => {
   switch5 = 0;
   res.send("switch 2 is off");
 });
+app.get("/status2", (req, res) => {
+  if (switch4 == 0 && switch5 == 0) {
+    res.send("0");
+  } else if (switch4 == 1 && switch5 == 0) {
+    res.send("10");
+  } else if (switch4 == 1 && switch5 == 1) {
+    res.send("11");
+  } else if (switch4 == 0 && switch5 == 1) {
+    res.send("1");
+  }
+});
+app.get("/door1IsClosed", (req, res) => {
+  door1 = 1;
+  res.send("Door is Closed");
+});
+app.get("/door1IsOpen", (req, res) => {
+  door1 = 0;
+  res.send("Door is Open");
+});
+app.get("/door1Status", (req, res) => {
+  res.send(door1.toString());
+});
+app.get("/lock1Status", (req, res) => {
+  res.send(doorLock1.toString());
+});
+app.get("/openLock1", (req, res) => {
+  if (Boolean(seq)) {
+    res.send("Unlock Door..");
+    doorLock1 = 1;
+  } else {
+    doorLock1 = 0;
+    res.send("Lock is Open");
+  }
+});
+app.get("/closelock1", (req, res) => {
+  doorLock1 = 1;
+  res.send("Lock is Close");
+});
+app.get("/checkSeq1", (req, res) => {
+  res.send(seq1.toString());
+});
+app.get("/onSeq1", (req, res) => {
+  seq1 = 1;
+  res.send("LOCKED....!!");
+});
+app.get("/offSeq1", (req, res) => {
+  seq1 = 0;
+  res.send("UNLOCKED....!!");
+});
+app.get("/warning1", (req, res) => {
+  if (Boolean(seq1) && !Boolean(door1)) {
+    res.send("!!...Security is broken...!!");
+  }
+  res.send("All Ok...!!");
+});
+
+/* <-----------------Eye Blink Sensor Project GECB--------------------------> */
+let eye = 1;
+let driverSleep = 0;
+
+app.get("/eyeIsOpen", (req, res) => {
+  eye = 1;
+  res.send("Eye is Open");
+});
+app.get("/eyeIsClosed", (req, res) => {
+  eye = 0;
+  res.send("Eye is Close");
+});
+app.get("/driverInSleep", (req, res) => {
+  driverSleep = 1;
+  res.send("Driver is Sleaping");
+});
+app.get("/driverNotSleep", (req, res) => {
+  driverSleep = 0;
+  res.send("Driver is not Sleaping");
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running on port: http://localhost:${PORT}`)
+);
+
+/* <------------------Transformer Monitoring------------------------------> */
+let switch6 = 0;
+let switch7 = 0;
+let temp = 0;
+let Irms = 0;
+let Vrms = 0;
+let Powr = 0;
+let warning2 = "No Warning...!!";
 
 app.get("/onSwitch6", (req, res) => {
   switch6 = 1;
@@ -194,6 +379,42 @@ app.get("/offSwitch7", (req, res) => {
   switch7 = 0;
   res.send("Load 2 is off");
 });
+
+app.get("/status3", (req, res) => {
+  if (switch6 == 0 && switch7 == 0) {
+    res.send("0");
+  } else if (switch6 == 1 && switch7 == 0) {
+    res.send("10");
+  } else if (switch6 == 1 && switch7 == 1) {
+    res.send("11");
+  } else if (switch6 == 0 && switch7 == 1) {
+    res.send("1");
+  }
+  if ("temp" in req.query) {
+    temp = req.query.temp;
+    Irms = req.query.Irms;
+    Vrms = req.query.Vrms;
+    Powr = (Irms * Vrms) / 1000;
+    warning2 = req.query.Warning;
+  }
+});
+
+/* <------------------Energy Meater Vivek---------------------------------> */
+
+let switch8 = 0;
+let switch9 = 0;
+let switch10 = 0;
+let switch11 = 0;
+
+let cs1Val = 0.0;
+let cs2Val = 0.0;
+let vsVal = 0.0;
+
+let prvTime = 0.0;
+let crtTime = 0.0;
+let prvPowr = 0.0;
+let powr = 0.0;
+let energy = 0.0;
 
 app.get("/onSwitch8", (req, res) => {
   switch8 = 1;
@@ -226,61 +447,6 @@ app.get("/offSwitch10", (req, res) => {
 app.get("/offSwitch11", (req, res) => {
   switch11 = 0;
   res.send("Switch 1 is off");
-});
-
-app.get("/status", (req, res) => {
-  if (switch1 == 0 && switch2 == 0) {
-    res.send("0");
-  } else if (switch1 == 1 && switch2 == 0) {
-    res.send("10");
-  } else if (switch1 == 1 && switch2 == 1) {
-    res.send("11");
-  } else if (switch1 == 0 && switch2 == 1) {
-    res.send("1");
-  }
-});
-
-app.get("/status1", (req, res) => {
-  if (switch3 == 0 && sensor == 0) {
-    res.send("0");
-  } else if (switch3 == 1 && sensor == 0) {
-    res.send("10");
-  } else if (switch3 == 1 && sensor == 1) {
-    res.send("11");
-  } else if (switch3 == 0 && sensor == 1) {
-    res.send("1");
-  }
-});
-
-app.get("/status2", (req, res) => {
-  if (switch4 == 0 && switch5 == 0) {
-    res.send("0");
-  } else if (switch4 == 1 && switch5 == 0) {
-    res.send("10");
-  } else if (switch4 == 1 && switch5 == 1) {
-    res.send("11");
-  } else if (switch4 == 0 && switch5 == 1) {
-    res.send("1");
-  }
-});
-
-app.get("/status3", (req, res) => {
-  if (switch6 == 0 && switch7 == 0) {
-    res.send("0");
-  } else if (switch6 == 1 && switch7 == 0) {
-    res.send("10");
-  } else if (switch6 == 1 && switch7 == 1) {
-    res.send("11");
-  } else if (switch6 == 0 && switch7 == 1) {
-    res.send("1");
-  }
-  if ("temp" in req.query) {
-    temp = req.query.temp;
-    Irms = req.query.Irms;
-    Vrms = req.query.Vrms;
-    Powr = (Irms * Vrms) / 1000;
-    warning2 = req.query.Warning;
-  }
 });
 
 app.get("/status4", (req, res) => {
@@ -342,145 +508,3 @@ app.get("/getData", (req, res) => {
       warning2
   );
 });
-
-app.get("/updateSwitch11", (req, res) => {
-  switch1 = 1;
-  switch2 = 1;
-  res.send("updated");
-});
-app.get("/updateSwitch10", (req, res) => {
-  switch1 = 1;
-  switch2 = 0;
-  res.send("updated");
-});
-app.get("/updateSwitch01", (req, res) => {
-  switch1 = 0;
-  switch2 = 1;
-  res.send("updated");
-});
-app.get("/updateSwitch00", (req, res) => {
-  switch1 = 0;
-  switch2 = 0;
-  res.send("updated");
-});
-
-app.get("/doorIsClosed", (req, res) => {
-  door = 1;
-  res.send("Door is Closed");
-});
-
-app.get("/doorIsOpen", (req, res) => {
-  door = 0;
-  res.send("Door is Open");
-});
-
-app.get("/door1IsClosed", (req, res) => {
-  door1 = 1;
-  res.send("Door is Closed");
-});
-app.get("/door1IsOpen", (req, res) => {
-  door1 = 0;
-  res.send("Door is Open");
-});
-
-app.get("/doorStatus", (req, res) => {
-  res.send(door.toString());
-});
-
-app.get("/door1Status", (req, res) => {
-  res.send(door1.toString());
-});
-
-app.get("/lockStatus", (req, res) => {
-  res.send(doorLock.toString());
-});
-
-app.get("/lock1Status", (req, res) => {
-  res.send(doorLock1.toString());
-});
-
-app.get("/openLock", (req, res) => {
-  if (Boolean(seq)) {
-    res.send("Unlock Door..");
-    doorLock = 1;
-  } else {
-    doorLock = 0;
-    res.send("Lock is Open");
-  }
-});
-
-app.get("/openLock1", (req, res) => {
-  if (Boolean(seq)) {
-    res.send("Unlock Door..");
-    doorLock1 = 1;
-  } else {
-    doorLock1 = 0;
-    res.send("Lock is Open");
-  }
-});
-
-app.get("/closelock", (req, res) => {
-  doorLock = 1;
-  res.send("Lock is Close");
-});
-app.get("/checkSeq", (req, res) => {
-  res.send(seq.toString());
-});
-app.get("/onSeq", (req, res) => {
-  seq = 1;
-  res.send("LOCKED....!!");
-});
-app.get("/offSeq", (req, res) => {
-  seq = 0;
-  res.send("UNLOCKED....!!");
-});
-app.get("/warning", (req, res) => {
-  if (Boolean(seq) && !Boolean(door)) {
-    res.send("!!...Security is broken...!!");
-  }
-  res.send("All Ok...!!");
-});
-
-app.get("/closelock1", (req, res) => {
-  doorLock1 = 1;
-  res.send("Lock is Close");
-});
-app.get("/checkSeq1", (req, res) => {
-  res.send(seq1.toString());
-});
-app.get("/onSeq1", (req, res) => {
-  seq1 = 1;
-  res.send("LOCKED....!!");
-});
-app.get("/offSeq1", (req, res) => {
-  seq1 = 0;
-  res.send("UNLOCKED....!!");
-});
-app.get("/warning1", (req, res) => {
-  if (Boolean(seq1) && !Boolean(door1)) {
-    res.send("!!...Security is broken...!!");
-  }
-  res.send("All Ok...!!");
-});
-
-app.get("/HomeStatus", (req, res) => {
-  gasLeakage = req.query.gasLeakage;
-  smoke = req.query.smoke;
-  fire = req.query.fire;
-  res.send(window);
-});
-app.get("/HomeStatus1", (req, res) => {
-  res.send(gasLeakage + smoke + fire);
-});
-app.get("/openWindow", (req, res) => {
-  window = "openWindow";
-  res.send("Window is open");
-});
-app.get("/closeWindow", (req, res) => {
-  window = "closeWindow";
-  res.send("Window is Close");
-});
-
-app.listen(PORT, () =>
-  console.log(`Server running on port: http://localhost:${PORT}`)
-);
