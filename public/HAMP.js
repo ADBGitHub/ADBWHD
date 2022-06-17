eba9f5f6281f435c820fe662961a07594af996c1
Abhinay Dev Berma <abhinaydevberma@gmail.com>
2022-06-17 16:23:22 +0530

    updated

--- public/HAMP.js
+++ public/HAMP.js
@@ -12,3 +12,28 @@ function onClickHandler() {
   };
   xhr.send();
 }
+
+function getData() {
+  return Math.random();
+}
+
+Plotly.plot("chart_div", [
+  {
+    y: [getData()],
+    type: "line",
+  },
+]);
+
+var cnt = 0;
+
+// setInterval(function () {
+//   Plotly.extendTraces("chart_div", { y: [[getData()]] }, [0]);
+//   cnt++;
+//   if (cnt > 500) {
+//     Plotly.relayout("chart_div", {
+//       xaxis: {
+//         range: [cnt - 500, cnt],
+//       },
+//     });
+//   }
+// }, 15);
