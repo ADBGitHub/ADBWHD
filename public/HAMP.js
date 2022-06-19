80f6df662954a1e5cf4a2adbe92529b013bb7677
Abhinay Dev Berma <abhinaydevberma@gmail.com>
2022-06-19 18:31:00 +0530

    updated

--- public/HAMP.js
+++ public/HAMP.js
@@ -152,8 +152,8 @@ setInterval(function () {
     adbc1 = responceData.adbc1;
     adbc2 = responceData.adbc2;
     adbp = responceData.adbp;
-    adbpA1 = parseFloat(adbc1) * parseFloat(adbv);
-    adbpA2 = parseFloat(adbc2) * parseFloat(adbv);
+    adbpA1 = parseFloat(adbc1).toFixed(2) * parseFloat(adbv).toFixed(2);
+    adbpA2 = parseFloat(adbc2).toFixed(2) * parseFloat(adbv).toFixed(2);
     time = responceData.time;
     s1 = responceData.switch1;
     s2 = responceData.switch2;
@@ -178,7 +178,8 @@ setInterval(function () {
   xhr.send();
 
   vVal.innerText = adbv + " Volt";
-  cVal.innerText = parseFloat(adbc1) + parseFloat(adbc2) + " mA";
+  cVal.innerText =
+    parseFloat(adbc1).toFixed(2) + parseFloat(adbc2).toFixed(2) + " mA";
   pVal.innerText = adbp + " W";
   app1Vval.innerText = adbv + " V";
   app1Cval.innerText = adbc1 + " mA";
